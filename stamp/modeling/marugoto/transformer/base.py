fixed parameters for frozen base + weighted average options

stamp/modeling/marugoto/transformer/base.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 
 from .cobra import MambaMILmocoWrap
 from .data import make_dataset, SKLearnEncoder
-from .SophiaMIL import WagnerMIL
+from .swMIL import WagnerMIL
 
 
 __all__ = ['train', 'deploy']
@@ -38,6 +38,8 @@ def train(
     cores: int = 8,
     model_arch: str = "sophia_mil",
     pretrained: Optional[Path] = None,
+    freeze_base: Optional[bool] = None,
+    weighted_avg: Optional[bool] = None,
     plot: bool = False
 ) -> Learner:
     """Train a MLP on image features.
@@ -95,7 +97,8 @@ def train(
         input_dim = embed_dim
         layer = 2
         att_dim = 256
-        model = MambaMILmocoWrap(embed_dim,c_dim,input_dim,layer=layer,att_dim=att_dim) 
+        model = MambaMILmocoWrap(embed_dim,c_dim,input_dim,layer=layer,att_dim=att_dim,
+                                 get_weighted_avg=weighted_avg,freeze_base=freeze_base) 
         if pretrained:
             chkpt = torch.load(pretrained)
             if "state_dict" in list(chkpt.keys()):
@@ -104,7 +107,10 @@ def train(
             print(f"Loading state dict from {pretrained}")
             msg=model.load_state_dict(base_enc)
             print(msg)
-        model.proj = nn.Linear(1280,len(target_enc.categories_[0]))
+        if weighted_avg:
+            model.proj = nn.Linear(feature_dim,len(target_enc.categories_[0]))
+        else:
+            model.proj = nn.Linear(embed_dim,len(target_enc.categories_[0]))
             
     # TODO:
     # maybe increase mlp_dim? Not necessary 4*dim, but maybe a bit?
@@ -163,7 +169,7 @@ def deploy(
     test_df: pd.DataFrame, learn: Learner, *,
     target_label: Optional[str] = None,
     cat_labels: Optional[Sequence[str]] = None, cont_labels: Optional[Sequence[str]] = None,
-    device: str = 'cpu'
+    device: str = 'cpu', 
 ) -> pd.DataFrame:
     assert test_df.PATIENT.nunique() == len(test_df), 'duplicate patients!'
     #assert (len(add_label)

stamp/modeling/marugoto/transformer/cobra.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append("../")
 sys.path.append("../ssl")
 sys.path.append("../../")
-sys.path.append("../../Mamba2MIL")
+sys.path.append("/mnt/bulk-neptune/timlenz/tumpe/Mamba2MIL")
 #sys.path.append("../../Mamba2MIL/models")
 #sys.path.append("../../Mamba2MIL/mamba")
 from Mamba2MIL.models.MambaMIL import MambaMIL
@@ -26,7 +26,7 @@ class Embed(nn.Module):
         return self.head(x)
 
 class MambaMILmocoWrap(nn.Module):
-    def __init__(self,embed_dim, c_dim,input_dim=1536,layer=4,att_dim=256):
+    def __init__(self,embed_dim, c_dim,input_dim=1536,layer=4,att_dim=256,get_weighted_avg=False,freeze_base=False):
         super().__init__()
         self.embed = nn.ModuleDict({#"512":Embed(512,embed_dim),
                                     "768":Embed(768,embed_dim),
@@ -47,15 +47,17 @@ class MambaMILmocoWrap(nn.Module):
         )
 
         #self.classifier = nn.Linear(embed_dim,num_classes)
-        for param in self.mamba_mil.parameters():
-            param.requires_grad = False
-        for param in self.mamba_mil.attention.parameters():
-            param.requires_grad = True
-        for param in self.mamba_mil.classifier.parameters():
-            param.requires_grad = True
+        if freeze_base:
+            for param in self.mamba_mil.parameters():
+                param.requires_grad = False
+            for param in self.mamba_mil.attention.parameters():
+                param.requires_grad = True
+            for param in self.mamba_mil.classifier.parameters():
+                param.requires_grad = True
+                
+        self.get_weighted_avg = get_weighted_avg
             
-    def forward(self, x,lens,get_tile_embs=False,get_weighted_avg=True):
-       
+    def forward(self, x,lens,get_tile_embs=False):
         
         #if self.use_embed:
         embs = self.embed[str(x.shape[-1])](x)
@@ -63,7 +65,7 @@ class MambaMILmocoWrap(nn.Module):
         #    embs = x
         if get_tile_embs:
             return self.mamba_mil(embs)[1]
-        if get_weighted_avg:
+        if self.get_weighted_avg:
             A = self.mamba_mil(embs,hidden_states=False,return_weighting=True)[1]
             #print(f"{A.shape=}")
             return self.proj(torch.bmm(A,x).squeeze(1))